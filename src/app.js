44d8b71fe7463cb671900d21d8578890d529fefa
ferrerods <dsferrero@gmail.com>
2024-04-24 19:53:52 -0300

    Continuando con la creación del crud de USER

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const { config } = require('dotenv')
 config()
 
+const gymUserRoutes = require('./routes/gym.routes')
+
 const app = express()
+app.use(bodyParser.json())
+
+// TODO: connect data base
+
+//
+app.use('/users', gymUserRoutes)
+
 const port = process.env.PORT || 8080
 
 app.listen(port, () => {
--- src/routes/gym.routes.js
+++ src/routes/gym.routes.js
@@ -4,7 +4,34 @@ const router = express.Router()
 const gymUser = require('../models/gym.models')
 
 
-// get ALL users
+// MIDDLEWARE
+const getUsers = async (req, res, next) => {
+    let gUser;
+    const { id } = req.params;
+
+    if(!id.match()){
+        return res.status(404).json({
+            message : 'El ID del usuario no es válido'
+        })
+    }
+
+    try{
+        gUser = await gymUser.findById(id);
+        if(!gUser){
+            return res.status(404).json({
+                message : 'El ID del usuario no fue encontrado'
+            })
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+
+    res.gUser = gUser;
+    next()
+}
+
+// GET ALL users
 router.get('/', async (req, res) => {
     try {
         const users = await gymUser.find();
@@ -21,7 +48,17 @@ router.get('/', async (req, res) => {
     }
 })
 
-// create one user
+// GET user
+router.get('/:id', getUsers, async (req, res) => {
+    try {
+        res.json(res.gUser);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+// CREATE user
 router.post('/', async (req, res) => {
     const { typeUser, name, typeDoc, numberDoc, address, dateBirth, physicalFitness, active } = req?.body
 
@@ -48,3 +85,68 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: error.message })
     }
 })
+
+// PUT user
+router.put('/:id', getUsers, async (req, res) => {
+    try {
+        //typeUser, name, typeDoc, numberDoc, address, dateBirth, physicalFitness, active 
+        const _gUser = res.gUser
+        _gUser.typeUser = req.gUser.typeUser || gUser.typeUser
+        _gUser.name = req.body.name || gUser.name;
+        _gUser.typeDoc = req.body.typeDoc || gUser.typeDoc;
+        _gUser.numberDoc = req.body.numberDoc || gUser.numberDoc;
+        _gUser.address = req.body.address || gUser.address;
+        _gUser.physicalFitness = req.body.physicalFitness || gUser.physicalFitness;
+        _gUser.active = req.body.active || gUser.active;
+
+        const updatedUser = await gUser.save()
+        res.json(updatedUser)
+
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+    }
+})
+
+// PATCH user
+router.patch('/:id', getUsers, async (req, res) => {
+    try {
+        //typeUser, name, typeDoc, numberDoc, address, dateBirth, physicalFitness, active 
+        const _gUser = res.gUser
+        _gUser.typeUser = req.gUser.typeUser || gUser.typeUser
+        _gUser.name = req.body.name || gUser.name;
+        _gUser.typeDoc = req.body.typeDoc || gUser.typeDoc;
+        _gUser.numberDoc = req.body.numberDoc || gUser.numberDoc;
+        _gUser.address = req.body.address || gUser.address;
+        _gUser.physicalFitness = req.body.physicalFitness || gUser.physicalFitness;
+        _gUser.active = req.body.active || gUser.active;
+
+        const updatedUser = await gUser.save()
+        res.json(updatedUser)
+
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+    }
+})
+
+// DELETE user
+router.delete('/:id', getUsers, async (req, res) => {
+    try {
+        const _gUser = res.user
+        await _gUser.deleteOne({
+            _id: _gUser._id
+        });
+        res.json({
+            message: `El usuario ${_gUser.name} fue eliminado correctamente`
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }  
+  })
+
+module.exports = router
